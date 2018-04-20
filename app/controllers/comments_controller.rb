a6d595085d1baa8467828a7486c14e81e3a79a4f
Samantha Rabling <sjarillorabling@gmail.com>
2018-04-19 20:28:34 -0700

    starting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,20 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
   def create
+    @user = current_user
+    @event = Event.find(params[:id])
+    @comment = Comment.new(comment_params)
+    @comment.user = @user
+    @comment.event = @event
+    if @comment.save
+      redirect_to "/events/#{@event.id}"
+    else
+      flash[:error] = @comment.errors.full_messages
+      redirect_to "/events/#{@event.id}"
+    end
   end
 
-  def destroy
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
